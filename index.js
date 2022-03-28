b1a3fd0aab99daea81c08f9fe0920fee6d9282df
Eugene Ahn <eugeneahn125@gmail.com>
2022-03-28 18:34:44 -0500

    Created inquirer function and set up classes

--- index.js
+++ index.js
@@ -2,25 +2,67 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const jest = require("jest");
 
-
-const initialPrompt = async () => {
-    inquirer.prompt(
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your employee ID?",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email address?",
-    },
-)};
-
-
 const html = ``;
+const teamMembers = [];
+
+const questions = {
+    employee: [
+        {
+            type: "input",
+            name: "name",
+            message: "What is your employee's name?\n",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your employee's ID?\n",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your employee's email address?\n",
+        },
+    ],
+    manager: [
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Please provide your office number:\n",
+        }
+    ],
+    engineer: [
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's GitHub username?\n",
+        },
+    ],
+    intern: [
+        {
+            type: "input",
+            name: "school",
+            message: "What school does your intern attend?\n",
+        },
+    ],
+};
+
+const prompt = async (employeeType) => {
+    let userPrompt = await inquirer.prompt([...questions.employee, ...questions[employeeType]]);
+    teamMembers.push({...userPrompt, type: employeeType})
+    
+    const addMember = await inquirer.prompt({
+        type: "list",
+        name: "choice",
+        message: "Would you like to add another team member or finish building your team?",
+        choices: ["engineer", "intern", "Finish building my team"],
+    });
+
+    if (addMember.choice == "Finish building my team"){
+        console.log(teamMembers)
+        return;
+    } else {
+        await prompt(addMember.choice)
+    }
+
+};
+prompt("manager");
